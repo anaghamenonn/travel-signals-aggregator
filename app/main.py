1d4a03a6725d9af1cca445425bdd05aab6526ea6
Anagha PH <anagha.menon@deeptrace.in>
2026-01-17 16:10:10 +0530

    Prepare backened for deployment

--- app/main.py
+++ app/main.py
@@ -8,7 +8,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React dev server
+    allow_origins=[
+        "http://localhost:3000",
+        "https://travel-signals-aggregator.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
